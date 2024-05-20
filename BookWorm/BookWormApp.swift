7a4c40dc45c8d717d8e4f6f421543abac857e795
Marcus Benoit <benoit.marcus@gmail.com>
2024-05-20 16:55:26 +0200

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Marcus Benoit on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

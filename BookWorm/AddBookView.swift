0eeed67b512fbb35e5128d198726156112461192
Marcus Benoit <benoit.marcus@gmail.com>
2024-05-22 11:16:09 +0200

    Challenges 1 + 2 completed

---
+++ BookWorm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  BookWorm
+//
+//  Created by Marcus Benoit on 21.05.24.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
---
+++ BookWorm/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  BookWorm
+//
+//  Created by Marcus Benoit on 21.05.24.
+//
+
+import Foundation
